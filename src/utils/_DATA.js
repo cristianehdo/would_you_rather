1ddb4df0e7539c1df499bc5ba0ec22f0cb4a1652
cristianehdo <cristiane.hdo@gmail.com>
2021-03-12 16:00:05 +0000

    fix: Added avatar's urls

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: ,
+    avatarURL: 'https://res.cloudinary.com/dlyxj5ddk/image/upload/v1615563911/sarahedo_reuhhc.jpg',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: ,
+    avatarURL: 'https://res.cloudinary.com/dlyxj5ddk/image/upload/v1615564100/tyler_mcginnis_orj6qj.jpg',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: ,
+    avatarURL: 'https://res.cloudinary.com/dlyxj5ddk/image/upload/v1615564562/jhon_doe_e5tt0u.jpg',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
