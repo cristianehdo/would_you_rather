e821aa8d1505ae2f9142a7f276be489d5f750595
cristianehdo <cristiane.hdo@gmail.com>
2021-03-15 15:59:45 +0000

    feat: setAuthedUser on users list selected

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import users from './users'
 import questions from './questions'
 import { combineReducers } from 'redux'
+import authedUser from './authedUser'
 
 export default combineReducers({
   users,
-  questions
+  questions,
+  authedUser
 })
