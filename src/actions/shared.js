c2aeec181ec454a37a1ee176357a0bd2b82ae14a
cristianehdo <cristiane.hdo@gmail.com>
2021-03-15 18:41:15 +0000

    feat: Added answered array to question in order to easily find if user has answered to the question

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,13 +3,24 @@ import { receiveUsers } from './users'
 import { receiveQuestions } from './questions'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
+const formatedQuestions = (questions) => {
+  const formatedQuestions = {}
+  Object.keys(questions).forEach((questionId) => {
+    const question = questions[questionId]
+      formatedQuestions[questionId] = {
+        ...question,
+        answered: (question.optionOne.votes).concat(question.optionTwo.votes)
+      }
+  })
+  return formatedQuestions
+}
 
 export function handleInitialData () {
   return (dispatch) => {
     dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions }) => {
-        dispatch(receiveQuestions(questions))
+        dispatch(receiveQuestions(formatedQuestions(questions)))
         dispatch(receiveUsers(users))
         dispatch(hideLoading())
       })
