d1e6e34d66a21410a7ba81930b2a8205db6b2a8c
cristianehdo <cristiane.hdo@gmail.com>
2021-03-20 15:38:10 +0000

    feat: Render Tab component, udpdated mapToProps in order to prepare data to Tabs

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Tab from './Tab'
 
 class HomePage extends Component {
   render () {
+    const { tabs, users, questions } = this.props
     return(
       <div>
-        HOME
+        <Tab tabs={tabs} users={users} questions={questions} />
       </div>
     )
   }
@@ -15,12 +17,24 @@ function mapPropsToState({ users, authedUser, questions }) {
   authedUser = 'sarahedo' // to be removed
   const questionsIds = Object.keys(questions)
   const answeredQuestions = Object.keys(users[authedUser].answers)
+  const tabs = {
+    answeredQuestions: {
+      title: 'Answered questions',
+      questions: answeredQuestions,
+    },
+    unasweredQuestions: {
+      title: 'Unswered questions',
+    }
+  }
+  tabs.unasweredQuestions.questions = questionsIds.filter((questionId) => {
+    return !answeredQuestions.includes(questionId)
+  })
   return {
     users,
     authedUser,
-    answeredQuestions,
     questions,
-    questionsIds
+    questionsIds,
+    tabs,
   }
 }
 export default connect(mapPropsToState)(HomePage)
