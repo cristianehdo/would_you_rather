408b84f71db665d3823ee2d080ecc0c4ba5b3392
cristianehdo <cristiane.hdo@gmail.com>
2021-03-20 16:24:51 +0000

    feat: added state to Card in order to be able to handle question answer changes, added Card propaTypes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Button, Card as SemanticCard, Image } from 'semantic-ui-react'
+import QuestionOptions from './QuestionOptions'
 
-const Card = (props) => {
-  const { avatarSize, user, options, buttonColor, buttonLabel } = props
-  return (
-    <SemanticCard>
-      <SemanticCard.Content>
-        <Image
-          floated='left'
-          size={avatarSize}
-          src={user.avatarURL}
-        />
-        <SemanticCard.Header>{user.name}</SemanticCard.Header>
-          <SemanticCard.Description>
-            <strong>Would you rather...</strong>
-              {options}
-          </SemanticCard.Description>
-        </SemanticCard.Content>
-          <SemanticCard.Content extra>
-            <div className='ui bottom attached button'>
-              <Button basic color={buttonColor}>
-                {buttonLabel}
-              </Button>
-            </div>
-        </SemanticCard.Content>
-    </SemanticCard>
-  )
+class Card extends Component {
+  state = {
+    answer: 'optionOne',
+    questionPage: this.props.buttonLable === "submit answer"
+  }
+  handleButtonClick = (e) => {
+    if (this.state.questionPage) {
+      // set state
+      // call callback
+    } else {
+      // redirect to question page
+    }
+  }
+  handleAnswerChanged = (answer) => {
+    this.setState(answer)
+  }
+  render() {
+    const { avatarSize, user, options, buttonColor, buttonLabel  } = this.props
+    return (
+      <SemanticCard>
+        <SemanticCard.Content>
+          <Image
+            floated='left'
+            size={avatarSize}
+            src={user.avatarURL}
+          />
+          <SemanticCard.Header>{user.name}</SemanticCard.Header>
+            <SemanticCard.Description>
+              <strong>Would you rather...</strong>
+              <QuestionOptions
+                options={options}
+                onChange={this.handleAnswerChanged}
+                value={this.state.answer}
+               />
+            </SemanticCard.Description>
+          </SemanticCard.Content>
+            <SemanticCard.Content extra>
+              <div className='ui bottom attached button'>
+                <Button basic color={buttonColor} onClick={this.handleButtonClick}>
+                  {buttonLabel}
+                </Button>
+              </div>
+          </SemanticCard.Content>
+      </SemanticCard>
+    )
+  }
 }
 
+Card.propTypes = {
+  avatarSize: PropTypes.string,
+  buttonColor: PropTypes.string,
+  buttonLabel: PropTypes.string,
+  user: PropTypes.object,
+};
 export default Card
