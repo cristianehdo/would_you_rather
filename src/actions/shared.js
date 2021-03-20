7bc07aecd8fb877ee91b89de45840171b615ac96
cristianehdo <cristiane.hdo@gmail.com>
2021-03-20 12:31:07 +0000

    Revert "feat: Added answered array to question in order to easily find if user has answered to the question"
    This reverts commit c2aeec181ec454a37a1ee176357a0bd2b82ae14a.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,24 +3,13 @@ import { receiveUsers } from './users'
 import { receiveQuestions } from './questions'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
-const formatedQuestions = (questions) => {
-  const formatedQuestions = {}
-  Object.keys(questions).forEach((questionId) => {
-    const question = questions[questionId]
-      formatedQuestions[questionId] = {
-        ...question,
-        answered: (question.optionOne.votes).concat(question.optionTwo.votes)
-      }
-  })
-  return formatedQuestions
-}
 
 export function handleInitialData () {
   return (dispatch) => {
     dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions }) => {
-        dispatch(receiveQuestions(formatedQuestions(questions)))
+        dispatch(receiveQuestions(questions))
         dispatch(receiveUsers(users))
         dispatch(hideLoading())
       })
