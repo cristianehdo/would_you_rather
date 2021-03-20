e8c191998354c2b0b6ae87a44072b03055fff95d
cristianehdo <cristiane.hdo@gmail.com>
2021-03-20 21:20:40 +0000

    feat: Handle answer question, updated reducers with new actions

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,8 +1,8 @@
 import { getInitialData } from '../utils/api'
-import { receiveUsers } from './users'
-import { receiveQuestions } from './questions'
+import { receiveUsers, userAnsweredQuestion } from './users'
+import { receiveQuestions, questionAnswered } from './questions'
 import { showLoading, hideLoading } from 'react-redux-loading'
-
+import { saveQuestionAnswer } from '../utils/api'
 
 export function handleInitialData () {
   return (dispatch) => {
@@ -13,5 +13,22 @@ export function handleInitialData () {
         dispatch(receiveUsers(users))
         dispatch(hideLoading())
       })
+  }
+}
+
+export function handleAnswerQuestion ({qid, authedUser, answer}) {
+  return (dispatch, getState) => {
+    dispatch(showLoading())
+
+    return saveQuestionAnswer({
+      authedUser,
+      qid,
+      answer
+    })
+      .then(() => {
+        dispatch(questionAnswered({authedUser, qid, answer}))
+        dispatch(userAnsweredQuestion({authedUser, qid, answer}))
+      })
+      .then(() => dispatch(hideLoading()))
   }
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,23 @@
-import { RECEIVE_USERS } from '../actions/users'
+import { RECEIVE_USERS, USER_ANSWERED_QUESTION  } from '../actions/users'
 
-export default function users (state = {}, action) {
+export default function users (state = null, action) {
   switch(action.type) {
     case RECEIVE_USERS :
       return {
         ...state,
         ...action.users
+      }
+    case USER_ANSWERED_QUESTION :
+      const { authedUser, qid, answer } = action
+      return {
+        ...state,
+        [authedUser]: {
+          ...state[authedUser],
+          answers: {
+            ...state[authedUser].answers,
+            [qid]: answer
+          }
+        }
       }
     default:
       return state
