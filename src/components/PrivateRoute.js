1caeaad1bebf5f67ec10cff5227a8dd159b2c6a7
cristianehdo <cristiane.hdo@gmail.com>
2021-03-22 20:56:01 +0000

    feat: Create PrivateRoute in order to redirect user to sign in in case its needed

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
+
+function PrivateRoute({ isSignedIn, component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+          isSignedIn ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+}
+
+PrivateRoute.propTypes = {
+  component: PropTypes.object,
+  isSignedIn: PropTypes.bool,
+  location: PropTypes.object,
+};
+
+const mapStateToProps = ({ authedUser }) => {
+  return { isSignedIn: !!authedUser };
+};
+
+export default connect(mapStateToProps, {})(PrivateRoute);
