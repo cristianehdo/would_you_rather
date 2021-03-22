6a1bcc455314cb3946e00c96934df27d88b9dcc9
cristianehdo <cristiane.hdo@gmail.com>
2021-03-22 21:01:13 +0000

    feat: Added app router and main routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,41 @@
 import React, { Component, Fragment } from 'react'
+import { Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
-import Login from './Login'
 import LoadingBar from 'react-redux-loading'
+import Login from './Login'
+import HomePage from './HomePage'
+import Question from './Question'
+import PrivateRoute from './PrivateRoute'
+import NewQuestion from './NewQuestion'
+import { createBrowserHistory } from 'history'
+
+const newHistory = createBrowserHistory();
+
 
 class App extends Component {
   componentDidMount () {
     this.props.dispatch(handleInitialData())
   }
   render() {
-    console.log(this.props)
     return (
-
-      <Fragment>
-        <LoadingBar />
-        <div className='ui container'>
+      <Router history={newHistory}>
+        <Fragment>
+          <LoadingBar />
           {this.props.loading
             ? null
-            : <Login />
+            : <div className='ui raised very padded text container segment'>
+                <Switch>
+                  <Route path="/" exact component={Login} />
+                  <Route path="/login" exact component={Login} />
+                  <PrivateRoute path="/home" exact component={HomePage} />
+                  <PrivateRoute path="/questions/new" exact component={NewQuestion} />
+                  <PrivateRoute path="/questions/:id" exact component={Question} />
+                </Switch>
+              </div>
           }
-        </div>
-      </Fragment>
+        </Fragment>
+      </Router>
     )
   }
 }
