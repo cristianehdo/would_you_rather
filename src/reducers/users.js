834b626cbdb041a1fa2e36de9004268a21994827
cristianehdo <cristiane.hdo@gmail.com>
2021-04-01 20:56:56 +0100

    style: Adjust users reducer identation

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -2,24 +2,25 @@ import { RECEIVE_USERS, USER_ANSWERED_QUESTION  } from '../actions/users'
 
 export default function users (state = null, action) {
   switch(action.type) {
-    case RECEIVE_USERS :
-      return {
-        ...state,
-        ...action.users
-      }
-    case USER_ANSWERED_QUESTION :
-      const { authedUser, qid, answer } = action
-      return {
-        ...state,
-        [authedUser]: {
-          ...state[authedUser],
-          answers: {
-            ...state[authedUser].answers,
-            [qid]: answer
-          }
+  case RECEIVE_USERS:
+    return {
+      ...state,
+      ...action.users
+    }
+  case USER_ANSWERED_QUESTION: {
+    const { authedUser, qid, answer } = action
+    return {
+      ...state,
+      [authedUser]: {
+        ...state[authedUser],
+        answers: {
+          ...state[authedUser].answers,
+          [qid]: answer
         }
       }
-    default:
-      return state
+    }
+  }
+  default:
+    return state
   }
 }
