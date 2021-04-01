be4b32bab0489b6965f91712c7df07d6632d4d4c
cristianehdo <cristiane.hdo@gmail.com>
2021-04-01 21:01:15 +0100

    style: Remove semicolon, adjust identation

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Redirect, Route } from 'react-router-dom';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Redirect, Route } from 'react-router-dom'
 
 function PrivateRoute({ isSignedIn, component: Component, ...rest }) {
   return (
     <Route
       {...rest}
       render={props =>
-          isSignedIn ? <Component {...props} /> : <Redirect to="/login" />
+        isSignedIn ? <Component {...props} /> : <Redirect to="/login" />
       }
     />
-  );
+  )
 }
 
 PrivateRoute.propTypes = {
   component: PropTypes.object,
   isSignedIn: PropTypes.bool,
   location: PropTypes.object,
-};
+}
 
 const mapStateToProps = ({ authedUser }) => {
-  return { isSignedIn: !!authedUser };
-};
+  return { isSignedIn: !!authedUser }
+}
 
-export default connect(mapStateToProps, {})(PrivateRoute);
+export default connect(mapStateToProps, {})(PrivateRoute)
