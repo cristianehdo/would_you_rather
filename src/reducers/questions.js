fa421dc53c37377a1e1481f4e169a9d5d7f2f847
cristianehdo <cristiane.hdo@gmail.com>
2021-04-01 21:04:10 +0100

    style: Adjust identation

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -2,31 +2,32 @@ import { RECEIVE_QUESTIONS, QUESTION_ANSWERED, ADD_QUESTION } from '../actions/q
 
 export default function questions (state = {}, action) {
   switch(action.type) {
-    case RECEIVE_QUESTIONS :
-      return {
-        ...state,
-        ...action.questions
-      }
-    case QUESTION_ANSWERED :
-      const { authedUser, qid, answer } = action
-      return {
-        ...state,
-        [qid]: {
-          ...state[qid],
-          [answer]: {
-            ...state[qid][answer],
-            votes: state[qid][answer].votes.concat([authedUser])
-          }
+  case RECEIVE_QUESTIONS:
+    return {
+      ...state,
+      ...action.questions
+    }
+  case QUESTION_ANSWERED: {
+    const { authedUser, qid, answer } = action
+    return {
+      ...state,
+      [qid]: {
+        ...state[qid],
+        [answer]: {
+          ...state[qid][answer],
+          votes: state[qid][answer].votes.concat([authedUser])
         }
       }
-    case ADD_QUESTION :
-      return {
-        ...state,
-        [action.question.id]: {
-          ...action.question
-        }
+    }
+  }
+  case ADD_QUESTION :
+    return {
+      ...state,
+      [action.question.id]: {
+        ...action.question
       }
-    default:
-      return state
+    }
+  default:
+    return state
   }
 }
