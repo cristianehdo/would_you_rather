0dc4a8a1018339b0bfbf7e0c4f005de38f31bf0b
cristianehdo <cristiane.hdo@gmail.com>
2021-04-10 14:07:09 +0100

    feat: Add withRouter in order to get question id from router match

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
 import Card from './Card'
 import { handleAnswerQuestion } from '../actions/shared'
 
 class Question extends Component {
-  handleButtonClick = (selectedOption) => {
-    const { dispatch, question, authedUser } = this.props
+  handleButtonClick = (selectedOption, id) => {
+    const { dispatch, authedUser } = this.props
     dispatch(handleAnswerQuestion({
-      qid: question.id,
+      qid: id,
       answer: selectedOption,
       authedUser
     }))
   }
   render () {
-    const { question, users } = this.props
+    const { questions, users, match } = this.props
+    const question = questions[match.params.id]
     return (
       <div className="ui list">
         <div className="item" key={question.id} >
@@ -35,17 +37,17 @@ class Question extends Component {
 Question.propTypes = {
   questionsIds: PropTypes.array,
   users: PropTypes.object,
-  question: PropTypes.object,
+  questions: PropTypes.object,
+  match: PropTypes.object,
   dispatch: PropTypes.func,
   authedUser: PropTypes.string,
 }
 
-function mapStateToProps({ authedUser, users, questions }, { id }) {
-  const question = questions[id]
+function mapStateToProps({ authedUser, users, questions }) {
   return {
     authedUser,
-    question: question,
+    questions,
     users
   }
 }
-export default connect(mapStateToProps)(Question)
+export default withRouter(connect(mapStateToProps)(Question))
