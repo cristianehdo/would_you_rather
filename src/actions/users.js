1d99349402fd97cca3c4bbc34dd5f4b137b4887a
cristianehdo <cristiane.hdo@gmail.com>
2021-04-11 11:59:05 +0100

    feat: Add userAddQuestion action

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,6 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 export const USER_ANSWERED_QUESTION = 'USER_ANSWERED_QUESTION'
+export const USER_ADDED_QUESTION = 'USER_ADDED_QUESTION'
 
 export function receiveUsers(users) {
   return {
@@ -14,5 +15,13 @@ export function userAnsweredQuestion({qid, authedUser, answer}) {
     qid,
     authedUser,
     answer
+  }
+}
+
+export function userAddedQuestion({id, author}) {
+  return {
+    type: USER_ADDED_QUESTION,
+    qid: id,
+    authedUser: author
   }
 }
