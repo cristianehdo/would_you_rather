0950e747c99d8db281adcec3fad6b18232ae6930
cristianehdo <cristiane.hdo@gmail.com>
2021-04-11 13:06:59 +0100

    feat: Add NotFound page

---
+++ src/components/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NotFound = () => {
+  return(
+    <div className="ui grid middle aligned segment red inverted" style={{height: '100vh', margin: 0}}>
+      <div className="ui column center aligned">
+        <div className="ui inverted statistic">
+          <div className="value">404</div>
+          <div className="label">Error</div>
+          <div className="label">Not found</div>
+        </div>
+
+        <div className="ui message red inverted">
+          <p>Please log in.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
